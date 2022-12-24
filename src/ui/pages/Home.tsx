d5aac423effc4e2bb2272d355e528cb847789e6a
Edvaldo Afonso <eadafonso@gmail.com>
2022-12-24 08:29:25 +0100

    chore: :tada: update styles

--- src/ui/pages/Home.tsx
+++ src/ui/pages/Home.tsx
@@ -1,65 +1,55 @@
 import { AddIcon } from "@chakra-ui/icons";
-import {
-    Button,
-    Stack,
-    useDisclosure,
-} from "@chakra-ui/react";
+import { Box, Button, Stack, useDisclosure } from "@chakra-ui/react";
 import React from "react";
-import styled from "styled-components";
+
 import ProductCard from "../../components/ProductCard";
 import { ProductType } from "../../models/ProductType";
 import useHomeViewModel from "../viewmodels/HomeViewModel";
 import AddProduct from "./AddProduct";
 
 export default function Home(): JSX.Element {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { saveProduct, products, getProducts } = useHomeViewModel();
 
-    const { isOpen, onOpen, onClose } = useDisclosure();
-    const { saveProduct, products, getProducts } = useHomeViewModel();
-
-    // TODO - Look for a better implementation
-    function RenderProduct(): any {
-        let element: Array<JSX.Element> = []
-        for (const product of products) {
-            element.push(<ProductCard key={product.productName} product={product} />)
-        }
-        return element;
+  // TODO - Look for a better implementation
+  function RenderProduct(): any {
+    let element: Array<JSX.Element> = [];
+    for (const product of products) {
+      element.push(<ProductCard key={product.productName} product={product} />);
     }
+    return element;
+  }
 
-    React.useEffect(() => {
-        getProducts();
-    }, []);
+  React.useEffect(() => {
+    getProducts();
+  }, []);
 
-    function addProduct() {
-        onOpen();
-    }
+  function saveProducts(product: ProductType) {
+    saveProduct(product);
+    products.push(product);
+  }
 
-    function saveProducts(product: ProductType) {
-        saveProduct(product);
-        products.push(product);
-    }
-
-    return (
-        <>
-            <Stack direction='row' spacing={4}>
-                <Button onClick={addProduct} leftIcon={<AddIcon />} colorScheme='teal' variant='solid'>
-                    Add Product
-                </Button>
-            </Stack>
-            <AddProduct
-                open={isOpen}
-                close={onClose}
-                onSave={saveProducts}
-            />
-            <ProductsContent>
-                <RenderProduct />
-            </ProductsContent>
-        </>
-    )
+  return (
+    <>
+      <Stack direction="row" spacing={4}>
+        <Button
+          onClick={() => onOpen()}
+          leftIcon={<AddIcon />}
+          colorScheme="teal"
+          variant="solid"
+        >
+          Add Product
+        </Button>
+      </Stack>
+      <AddProduct open={isOpen} close={onClose} onSave={saveProducts} />
+      <Box
+        width="100%"
+        display={"grid"}
+        padding="20px 0"
+        gridTemplateColumns="auto"
+      >
+        <RenderProduct />
+      </Box>
+    </>
+  );
 }
-
-const ProductsContent = styled.section`
-    width: 100%;
-    padding: 20px 0;
-    display: grid;
-    grid-template-columns: auto auto auto auto auto;
-`;
